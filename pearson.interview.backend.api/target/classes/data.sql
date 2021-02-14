cc214ccad21d83c110f08387875f960499a5d048
HansikaPerera <madhuhanz@gmail.com>
2021-02-14 11:38:45 +0300

    Ignore target folder

--- pearson.interview.backend.api/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO COUNTRY (name) VALUES ('Sri Lanka');
-INSERT INTO COUNTRY (name) VALUES ('India');
-INSERT INTO COUNTRY (name) VALUES ('America');
-INSERT INTO COUNTRY (name) VALUES ('Russia');
-INSERT INTO COUNTRY (name) VALUES ('Australia');
