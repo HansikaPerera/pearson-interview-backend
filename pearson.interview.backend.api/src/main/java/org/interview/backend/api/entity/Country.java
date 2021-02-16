7c23ecf4c968e0ddaeeec879f20bc60629802281
HansikaPerera <madhuhanz@gmail.com>
2021-02-17 00:13:01 +0300

    Remove uuid field in country table

--- pearson.interview.backend.api/src/main/java/org/interview/backend/api/entity/Country.java
+++ pearson.interview.backend.api/src/main/java/org/interview/backend/api/entity/Country.java
@@ -9,14 +9,6 @@ import javax.persistence.*;
 public class Country {
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    @Column
-    private String id;
-
     @Column
     private String name;
 
@@ -25,16 +17,6 @@ public class Country {
 
     @Column
     private String capital;
-
-    public String getId() {
-
-        return id;
-    }
-
-    public void setId(String id) {
-
-        this.id = id;
-    }
 
     public String getName() {
 
