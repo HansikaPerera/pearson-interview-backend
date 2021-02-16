18c8f640001d1e1b022e4f918cb8a9e60bf46e69
HansikaPerera <madhuhanz@gmail.com>
2021-02-16 10:12:59 +0300

    Add more country data

--- pearson.interview.backend.api/src/main/resources/data.sql
+++ pearson.interview.backend.api/src/main/resources/data.sql
@@ -1,5 +1,7 @@
-INSERT INTO COUNTRY (name) VALUES ('Sri Lanka');
-INSERT INTO COUNTRY (name) VALUES ('India');
-INSERT INTO COUNTRY (name) VALUES ('America');
-INSERT INTO COUNTRY (name) VALUES ('Russia');
-INSERT INTO COUNTRY (name) VALUES ('Australia');
+INSERT INTO COUNTRY (name, population, capital) VALUES ('Sri Lanka', 21800000, 'Colombo');
+INSERT INTO COUNTRY (name, population, capital) VALUES ('India', 1380004385, 'New Delhi');
+INSERT INTO COUNTRY (name, population, capital) VALUES ('America', 328200000, 'Washington, D.C.');
+INSERT INTO COUNTRY (name, population, capital) VALUES ('Russia', 144400000, 'Moscow');
+INSERT INTO COUNTRY (name, population, capital) VALUES ('Australia', 25360000, 'Canberra');
+INSERT INTO COUNTRY (name, population, capital) VALUES ('Pakistan', 216600000, 'Islamabad');
+
